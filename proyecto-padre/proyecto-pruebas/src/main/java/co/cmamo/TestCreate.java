c8d363867e8e06ea3164727b5a7003d6470f7524
Joshua <ccquicenol@uqvirtual.edu.co>
2019-05-09 16:22:41 -0500

    Test de lectura y archivos JSON
    - Se crearon los archivos JSON de cada clase
    - Se crearon los Test 
    - Se testeo la lectura de las clases principales

---
+++ proyecto-padre/proyecto-pruebas/src/main/java/co/cmamo/TestCreate.java
@@ -0,0 +1,43 @@
+package co.cmamo;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class TestCreate {
+	
+	/**
+	 * instancia para realizar las transaciones con las entidades
+	 */
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	/**
+	 * general el archivo de depliegue de pruebas
+	 * 
+	 * @return genera un archivo de configuracion web
+	 */
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Persona.class.getPackage())
+				.addAsResource("persistenceForTest.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Test
+	public void persistFamiliaTest() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ proyecto-padre/proyecto-pruebas/src/main/java/co/cmamo/TestDelete.java
@@ -0,0 +1,40 @@
+package co.cmamo;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+
+public class TestDelete {
+	
+	/**
+	 * instancia para realizar las transaciones con las entidades
+	 */
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	/**
+	 * general el archivo de depliegue de pruebas
+	 * 
+	 * @return genera un archivo de configuracion web
+	 */
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap.create(WebArchive.class, "test.war").addPackage(Persona.class.getPackage())
+				.addAsResource("persistenceForTest.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
