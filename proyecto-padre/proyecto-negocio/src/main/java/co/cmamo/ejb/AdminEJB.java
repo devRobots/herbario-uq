712bff4ecfc068cddf9444c0e69d3716c24d2ec5
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2019-05-16 21:08:37 -0500

    EJB
    Hasta aqui faltan algunos queries

---
+++ proyecto-padre/proyecto-negocio/src/main/java/co/cmamo/ejb/AdminEJB.java
@@ -0,0 +1,20 @@
+package co.cmamo.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class AdminEJB
+ */
+@Stateless
+@LocalBean
+public class AdminEJB implements AdminEJBRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public AdminEJB() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ proyecto-padre/proyecto-negocio/src/main/java/co/cmamo/ejb/AdminEJBRemote.java
@@ -0,0 +1,8 @@
+package co.cmamo.ejb;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface AdminEJBRemote {
+
+}
