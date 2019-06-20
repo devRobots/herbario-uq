f6711cb223cdac8ca6d5fbc8ac432c79fe7f5f3f
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2019-06-20 17:40:20 -0500

    Pequeños detalles, y email
    mensaje de validacion puesta, correo arreglado "" entre comillas revisar XD, imagen sigue sin ocultarse.

--- herbario-uq-padre/herbario-uq-escritorio/src/main/java/co/cmamo/controlador/EdicionTaxonomiaControlador.java
+++ herbario-uq-padre/herbario-uq-escritorio/src/main/java/co/cmamo/controlador/EdicionTaxonomiaControlador.java
@@ -33,65 +33,6 @@ public class EdicionTaxonomiaControlador {
 	
     @FXML
     private Button btnModificar;
- 
-    
-    public void setModo(boolean edicion) {
-    	btnAceptar.setVisible(!edicion);
-    	btnModificar.setVisible(edicion);
-    }
-
-    @FXML
-    void aceptar(ActionEvent event) {
-    	if (!txtNombre.getText().isEmpty()) {
-    		AdministradorDelegado delegado = controlador.getAdministradorDelegado();
-        	
-        	if (antecesor == null) {
-    			Familia familia = new Familia();
-    			familia.setNombre(txtNombre.getText());
-    			
-    			if (delegado.registrarFamilia(familia)) {
-    				controlador.agregarTaxonomiaALista((Familia) familia);
-    				Utilidades.mostrarMensaje("Registro", "Registro exitoso!!");
-    				escenarioEditar.close();
-    			}
-    			else {
-    				Utilidades.mostrarMensaje("Registro", "Error en registro!!", -2);
-    			}
-    		}	
-        	else	 if (antecesor instanceof Familia) {
-        		Genero genero = new Genero();
-    			genero.setNombre(txtNombre.getText());
-    			genero.setFamilia((Familia)antecesor);
-    			
-    			if (delegado.registrarGenero(genero)) {
-    				controlador.agregarTaxonomiaALista((Genero) genero);
-    				Utilidades.mostrarMensaje("Registro", "Registro exitoso!!");
-    				escenarioEditar.close();
-    			}
-    			else {
-    				Utilidades.mostrarMensaje("Registro", "Error en registro!!", -2);
-    			}
-    		}
-        	else {
-        		Planta planta = new Planta();
-    			planta.setEspecie(txtNombre.getText());
-    			planta.setGenero((Genero)antecesor);
-    			
-    			if (delegado.registrarPlanta(planta)) {
-    				controlador.agregarTaxonomiaALista((Planta) planta);
-    				Utilidades.mostrarMensaje("Registro", "Registro exitoso!!");
-    				escenarioEditar.close();
-    			}
-    			else {
-    				Utilidades.mostrarMensaje("Registro", "Error en registro!!", -2);
-    			}
-        	}
-
-	@FXML
-	private Button btnAceptar;
-
-	@FXML
-	private Button btnModificar;
 	
 	@FXML
 	private Button btnBuscar;
